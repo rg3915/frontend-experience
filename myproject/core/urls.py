e50639f5c578cf9f5d77b681bf3f3dc70ab5a937
rg3915 <rg3915@yahoo.com.br>
2017-07-15 18:27:32 -0300

    Adicionando Contato

--- myproject/core/urls.py
+++ myproject/core/urls.py
@@ -3,4 +3,5 @@ from myproject.core import views as v
 
 urlpatterns = [
     url(r'^$', v.home, name='home'),
+    url(r'^add/$', v.contact_add, name='contact_add'),
 ]
--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,6 +1,9 @@
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
-from .models import Contact
 from .forms import ContactForm
+from .models import Contact
 
 
 def home(request):
@@ -10,3 +13,23 @@ def home(request):
         'contact_form': ContactForm
     }
     return render(request, 'index.html', ctx)
+
+
+def contact_add(request):
+    if request.is_ajax() and request.method == 'POST':
+        form = ContactForm(request.POST)
+        if not form.is_valid():
+            error = {'error': form.errors}
+            return JsonResponse(error, status=422)
+
+        treatment = request.POST.get('treatment')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        f = form.save()
+        ctx = {'contacts': f.first_name}
+        return JsonResponse(ctx)
+    else:
+        messages.error(request, 'Favor preencher o nome do contato.')
+        return HttpResponseRedirect(reverse('home'))
