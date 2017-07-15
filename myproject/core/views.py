52244062f6b4866e3acae20066511d5716b4871d
rg3915 <rg3915@yahoo.com.br>
2017-07-15 17:29:04 -0300

    forms

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Contact
+from .forms import ContactForm
 
 
 def home(request):
     contacts = Contact.objects.all()
     ctx = {
         'contacts': contacts,
+        'contact_form': ContactForm
     }
     return render(request, 'index.html', ctx)
