acfe85bf970deccd79e38b229c04ca760a462743
rg3915 <rg3915@yahoo.com.br>
2017-07-18 23:43:20 -0300

    Edit contact

--- myproject/core/urls.py
+++ myproject/core/urls.py
@@ -4,5 +4,7 @@ from myproject.core import views as v
 urlpatterns = [
     url(r'^$', v.home, name='home'),
     url(r'^add/$', v.contact_add, name='contact_add'),
+    url(r'^(?P<pk>[^/]+)/json/$', v.contact_json, name='contact_json'),
+    url(r'^(?P<pk>[^/]+)/edit/$', v.contact_edit, name='contact_edit'),
     url(r'^(?P<pk>[^/]+)/delete/$', v.contact_delete, name='contact_delete'),
 ]
--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,7 +1,8 @@
 from django.contrib import messages
+from django.core import serializers
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, JsonResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render, get_object_or_404
 from .forms import ContactForm
 from .models import Contact
 
@@ -39,6 +40,35 @@ def contact_add(request):
         return JsonResponse(ctx)
     else:
         messages.error(request, 'Favor preencher o nome do contato.')
+        return HttpResponseRedirect(reverse('home'))
+
+
+def contact_json(request, pk):
+    contact = Contact.objects.filter(pk=pk)
+    data = serializers.serialize('json', contact)
+    return HttpResponse(data, content_type='application/json')
+
+
+def contact_edit(request, pk):
+    if request.is_ajax() and request.method == 'POST':
+        pk = request.POST.get('contact_edit_id')
+        contact = get_object_or_404(Contact, pk=pk)
+        # Usando instance não precisa pegar os campos um a um.
+        # form = CustomerForm(request.POST, instance=customer)
+
+        # if not form.is_valid():
+        #     error = {'error': form.errors}
+        #     return JsonResponse(error, status=422)
+
+        contact.treatment = request.POST.get('treatment')
+        contact.first_name = request.POST.get('first_name')
+        contact.last_name = request.POST.get('last_name')
+        contact.email = request.POST.get('email')
+        contact.phone = request.POST.get('phone')
+        contact.save()
+        response = {'status': 'update'}
+        return JsonResponse(response)
+    else:
         return HttpResponseRedirect(reverse('home'))
 
 
